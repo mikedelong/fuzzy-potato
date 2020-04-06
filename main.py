7583c06123d12534a5ce59b021cd8390157e9d39
mikedelong <mikedelong@outlook.com>
2020-04-06 09:17:52 -0400

    feature: shell code with one function we may eventually use

---
+++ main.py
@@ -0,0 +1,18 @@
+from logging import INFO
+from logging import basicConfig
+from logging import getLogger
+from time import time
+
+
+def float_color_to_hex(arg_float, arg_colormap):
+    color_value = tuple([int(255 * arg_colormap(arg_float)[index]) for index in range(3)])
+    return '#{:02x}{:02x}{:02x}'.format(color_value[0], color_value[1], color_value[2])
+
+
+if __name__ == '__main__':
+    time_start = time()
+    logger = getLogger(__name__)
+    basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=INFO)
+    logger.info('started.')
+
+    logger.info('total time: {:5.2f}s'.format(time() - time_start))
