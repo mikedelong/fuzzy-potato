491c3fa61508d83e60063ee3b076cafde38fa2b5
mikedelong <mikedelong@outlook.com>
2020-04-07 08:49:40 -0400

    feature: pick the target from a list of candidates; also, initialize the fig and ax to silence warnings in all cases

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from logging import INFO
 from logging import basicConfig
 from logging import getLogger
 from time import time
-import pandas as pd
+
 import matplotlib.pyplot as plt
-import plotly.express as px
+import pandas as pd
 import plotly.graph_objs as go
+
 
 def float_color_to_hex(arg_float, arg_colormap):
     color_value = tuple([int(255 * arg_colormap(arg_float)[index]) for index in range(3)])
@@ -24,17 +25,20 @@ if __name__ == '__main__':
     logger.info('data shape: {}'.format(df.shape))
     logger.info('data types: {}'.format(df.dtypes))
 
-    plots = ['matplotlib', 'plotly',]
+    plots = ['matplotlib', 'plotly', ]
     plot = plots[1]
+    ax, fig = None, None
     if plot == plots[0]:
         fig, ax = plt.subplots(figsize=(15, 10))
     elif plot == plots[1]:
         fig = go.Figure()
-        ax = None
 
-    target = 'countriesAndTerritories' # was 'geoId'
+    targets = ['countriesAndTerritories', 'geoId']
+    target = targets[0]
     count = 0
+    # todo can we sort by size instead of alphabetically by name?
     for geoId in df[target].unique():
+        # todo get exclusion list in line with target/targets
         if geoId not in {'CN', 'JPG11668', } and geoId is not None:
             geodf = df[df[target] == geoId][['dateRep', 'cases', 'deaths', 'popData2018']]
             if len(geodf) > 10:
@@ -46,14 +50,14 @@ if __name__ == '__main__':
                 geodf = geodf.sort_values(by=['dateRep'], axis=0, ascending=True)
                 logger.info('id: {} shape: {}'.format(geoId, geodf.shape))
                 geodf['case_cumsum'] = geodf['cases'].cumsum()
-                geodf['y'] = 10000*geodf['case_cumsum']/geodf['popData2018']
+                geodf['y'] = 10000 * geodf['case_cumsum'] / geodf['popData2018']
                 geodf['death_cumsum'] = geodf['deaths'].cumsum()
                 if geodf['case_cumsum'].max() > 5000:
                     if plot == plots[0]:
                         geodf.plot(x='dateRep', y='y', ax=ax, style='.', label=geoId, )
                     elif plot == plots[1]:
                         fig.add_trace(
-                            go.Scatter(x=geodf.dateRep, y=geodf.y, name=geoId.replace('_', ' '),)
+                            go.Scatter(x=geodf.dateRep, y=geodf.y, name=geoId.replace('_', ' '), )
                         )
 
     if plot == plots[0]:
