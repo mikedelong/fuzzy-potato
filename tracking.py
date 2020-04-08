784fd9e3dea9936a02f875c739784b2738eadfa0
mikedelong <mikedelong@outlook.com>
2020-04-08 08:37:52 -0400

    feature: make the date column a pandas date

--- tracking.py
+++ tracking.py
@@ -1,18 +1,9 @@
-# https://covidtracking.com/api/us/daily.csv
 from logging import INFO
 from logging import basicConfig
 from logging import getLogger
 from time import time
 
-import matplotlib.pyplot as plt
 import pandas as pd
-import plotly.graph_objs as go
-
-
-def float_color_to_hex(arg_float, arg_colormap):
-    color_value = tuple([int(255 * arg_colormap(arg_float)[index]) for index in range(3)])
-    return '#{:02x}{:02x}{:02x}'.format(color_value[0], color_value[1], color_value[2])
-
 
 if __name__ == '__main__':
     time_start = time()
@@ -23,8 +14,9 @@ if __name__ == '__main__':
     url = 'https://covidtracking.com/api/us/daily.csv'
     logger.info('reading from {}'.format(url))
     df = pd.read_csv(url)
+    date_format = '%Y%m%d'
+    df.date = pd.to_datetime(df.date.astype(int), format=date_format, )
     logger.info('data shape: {}'.format(df.shape))
     logger.info('data types: {}'.format(df.dtypes))
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
-
