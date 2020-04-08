c3663b9c45dbc4f0542812ec46394f503523365a
mikedelong <mikedelong@outlook.com>
2020-04-08 08:57:36 -0400

    feature: based on the positive data add the percent change and its 5-day moving mean

--- tracking.py
+++ tracking.py
@@ -17,6 +17,11 @@ if __name__ == '__main__':
     date_format = '%Y%m%d'
     df.date = pd.to_datetime(df.date.astype(int), format=date_format, )
     logger.info('data shape: {}'.format(df.shape))
+    df = df.sort_values(by='date')
     logger.info('data types: {}'.format(df.dtypes))
+
+    df = df[['date', 'positive']]
+    df['positive_change'] = df['positive'].pct_change()
+    df['rolling_positive_change'] = df['positive_change'].rolling(window=5, min_periods=5).mean()
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
