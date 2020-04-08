26d624197587cb3ed5d01135c51c3889348bb03f
mikedelong <mikedelong@outlook.com>
2020-04-08 09:21:55 -0400

    feature: calculate and report a bunch of rolling means and their one-day forecasts

--- tracking.py
+++ tracking.py
@@ -4,6 +4,7 @@ from logging import getLogger
 from time import time
 
 import pandas as pd
+from datetime import timedelta
 
 if __name__ == '__main__':
     time_start = time()
@@ -17,11 +18,16 @@ if __name__ == '__main__':
     date_format = '%Y%m%d'
     df.date = pd.to_datetime(df.date.astype(int), format=date_format, )
     logger.info('data shape: {}'.format(df.shape))
-    df = df.sort_values(by='date')
     logger.info('data types: {}'.format(df.dtypes))
 
-    df = df[['date', 'positive']]
-    df['positive_change'] = df['positive'].pct_change()
-    df['rolling_positive_change'] = df['positive_change'].rolling(window=5, min_periods=5).mean()
+    for window in range(1, 9):
+        positive_df = df[['date', 'positive']].copy(deep=True).sort_values(by='date')
+        positive_df['positive_change'] = positive_df['positive'].pct_change()
+        positive_df['rolling_positive_change'] = positive_df['positive_change'].rolling(window=window,
+                                                                                        min_periods=window, ).mean()
+        row = positive_df.tail(1).squeeze()
+        forecast_format = 'date: {}  {}d rm forecast {:.0f}'
+        logger.info(forecast_format.format((row['date'] + timedelta(days=1, )).date(), window,
+                                           row['positive'] * (1.0 + row['rolling_positive_change'])))
 
     logger.info('total time: {:5.2f}s'.format(time() - time_start))
