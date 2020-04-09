76f87a171ec3a35cdbd872b145277e4836aec3a6
mikedelong <mikedelong@outlook.com>
2020-04-09 08:47:53 -0400

    cleanup: remove an unused function

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.graph_objs as go
 
-
-def float_color_to_hex(arg_float, arg_colormap):
-    color_value = tuple([int(255 * arg_colormap(arg_float)[index]) for index in range(3)])
-    return '#{:02x}{:02x}{:02x}'.format(color_value[0], color_value[1], color_value[2])
-
-
 if __name__ == '__main__':
     time_start = time()
     logger = getLogger(__name__)
